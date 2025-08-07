58a1585a8629d4c1cac135c278656c2628e2d4f3
Marc Toensing <marc.toensing@gmail.com>
2025-08-07 19:01:38 +0200

    Initial commit

---
+++ build/noteblock/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '419bd7aa59960a69aa42');
---
+++ build/noteblock/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '44ecdf2035bfa8342799');
