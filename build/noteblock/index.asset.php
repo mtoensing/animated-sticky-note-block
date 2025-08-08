b08320ea886ba633ce93895c57aa3dc3eb298b51
Marc Toensing <marc.toensing@gmail.com>
2025-08-08 13:54:40 +0200

    better pin color

--- build/noteblock/index.asset.php
+++ build/noteblock/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'ebb137b0063a9bd312fe');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '0532a4b6470df24d2e68');
