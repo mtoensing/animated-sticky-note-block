627099a4d8ecd154a24c0e3e1dbad60a7c5802c4
Marc Toensing <marc.toensing@gmail.com>
2025-08-08 20:59:03 +0200

    better animation

--- build/noteblock/index.asset.php
+++ build/noteblock/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'f90d4a33dcf81c74ba32');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'fe1b495211476c99670f');
