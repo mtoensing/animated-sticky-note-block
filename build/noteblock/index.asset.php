bd00d84df5110cfca6a84e514badaafdd58e4859
Marc Toensing <marc.toensing@gmail.com>
2025-08-08 14:11:58 +0200

    refact

--- build/noteblock/index.asset.php
+++ build/noteblock/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '0532a4b6470df24d2e68');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '3a086f591725a169dd93');
