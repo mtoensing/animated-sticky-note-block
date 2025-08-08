4d7d7cd0dd6fde95c2a14fdeea1ab06d45adffad
Marc Toensing <marc.toensing@gmail.com>
2025-08-08 22:44:49 +0200

    Rename block init function for consistency
    Renamed the block initialization function and its hook from 'noteblock' to 'simplestickynote' to better match the plugin's naming and improve clarity.

--- simplestickynote.php
+++ simplestickynote.php
@@ -25,7 +25,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @see https://make.wordpress.org/core/2025/03/13/more-efficient-block-type-registration-in-6-8/
  * @see https://make.wordpress.org/core/2024/10/17/new-block-type-registration-apis-to-improve-performance-in-wordpress-6-7/
  */
-function create_block_noteblock_block_init() {
+function create_block_simplestickynote_block_init() {
 	/**
 	 * Registers the block(s) metadata from the `blocks-manifest.php` and registers the block type(s)
 	 * based on the registered block metadata.
@@ -57,4 +57,4 @@ function create_block_noteblock_block_init() {
 		register_block_type( __DIR__ . "/build/{$block_type}" );
 	}
 }
-add_action( 'init', 'create_block_noteblock_block_init' );
+add_action( 'init', 'create_block_simplestickynote_block_init' );
