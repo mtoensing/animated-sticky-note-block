ed2b5a3c849785d8ac1a8551fde41cea66fff7fe
Marc Toensing <marc.toensing@gmail.com>
2025-08-08 17:04:39 +0200

    reduced working version

--- build/noteblock/index.asset.php
+++ build/noteblock/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'd435325b93276e2cb416');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'e58b0de5c411418e5405');
